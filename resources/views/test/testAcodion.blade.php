2208e29b47d19261d50a106cb86033bd233ee793
maru <ct20216119@denpa.jp>
2024-01-18 15:03:07 +0900

    g

--- resources/views/test/testAcodion.blade.php
+++ resources/views/test/testAcodion.blade.php
@@ -49,11 +49,33 @@
           </div>
         </div>
         @endforeach
+        <p>
+        @foreach($questionCollections as $collection)
+        <div class="accordion_one">
+          <div class="accordion_header">{{ $collection->question_collection_name}}<div class="i_box"><i class="one_i"></i></div></div>
+          <div class="accordion_inner">
+            <div class="accordion_one">
+                @foreach($questionGroups as $group)
+                @if($group->question_collection_id == $collection->id)
+              <div class="accordion_header">{{ $group->question_group_names }}<div class="i_box"><i class="one_i"></i></div></div>
+                @endif
+              <div class="accordion_inner">
+                <div class="accordion_one">
+                  <div class="accordion_header">A_a</div>
+                  <div class="accordion_header">A_b</div>
+                </div>
+              </div>
+              @endforeach
+            </div>
+          </div>
+        </div>
+        @endforeach
     </div>
 
 
-        <p class="_a"><a href="https://125naroom.com/web/3046" target="_blank" class="link">View the note</a></p>
-      </div>
+
+
+
 
 
 
